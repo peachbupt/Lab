2952adafbb11a0e4b4db238297cdaa801886e3c9
jinxingh <jinxingh@vmware.com>
2017-02-06 22:28:44 +0800

    [microblog] - Part VI: Profile Page And Avatars.

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,10 +1,12 @@
 from app import db
-
+from hashlib import md5
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
 
     @property
     def is_authenticated(self):
@@ -23,6 +25,10 @@ class User(db.Model):
             return unicode(self.id)
         except NameError:
             return str(self.id)
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' \
+        % (md5(self.email.encode('utf-8')).hexdigest(), size)
 
     def __repr__(self):
         return "User {}".format(self.nickname)
--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from .forms import LoginForm
+from datetime import datetime
+from .forms import LoginForm, EditForm
 from .models import User
 
 @app.route('/')
@@ -65,17 +66,46 @@ def after_login(resp):
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user == None:
+        flash("User {} not found".format(nickname))
+        return redirect('index')
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html',
+        user = user,
+        posts = posts)
 
-
-
-
-
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
 
 
 
