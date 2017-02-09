d93f594900e50e3160a0daf9baa5c9ac7977fcad
jinxingh <jinxingh@vmware.com>
2017-02-09 09:09:36 +0800

    [microblog] - Part VIII, followers, contacts and friends

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,5 +1,11 @@
 from app import db
 from hashlib import md5
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(64), index = True, unique = True)
@@ -7,6 +13,13 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+        secondary = followers,
+        primaryjoin =(followers.c.follower_id == id),
+        secondaryjoin =(followers.c.followed_id == id),
+        backref = db.backref('followers', lazy = 'dynamic'),
+        lazy = 'dynamic')
+
 
     @property
     def is_authenticated(self):
@@ -44,6 +57,23 @@ class User(db.Model):
                 break
             version += 1
         return new_nickname
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Post.query.join(followers, (followers.c.followed_id == Post.user_id))\
+            .filter(followers.c.follower_id == self.id)\
+            .order_by(Post.timestamp.desc())
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
