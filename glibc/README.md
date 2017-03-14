5860f290e7050ccd8bca9c905090208f66c993d3
Peach <peach.bupt@gmail.com>
2017-03-13 19:25:14 -0700

    glibc hello world

---
+++ glibc/README.md
@@ -0,0 +1,2 @@
+	*1. apt-get install libgtk2.0-dev
+	*2. G_MESSAGES_DEBUG=all ./demo1_test
---
+++ glibc/demo1.cpp
@@ -0,0 +1,7 @@
+#include <glib.h>
+
+int main(int argc, char* argv[])
+{
+	g_debug("hello world\n");
+	return 0;
+}
