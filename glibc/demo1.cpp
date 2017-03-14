2f92636e49ebf1ee9c1465aa71fc2bd4d3b6beb7
Peach <peach.bupt@gmail.com>
2017-03-13 22:57:17 -0700

    glibc mainloop source test.

--- glibc/demo1.cpp
+++ glibc/demo1.cpp
@@ -1,7 +1,53 @@
 #include <glib.h>
+#include <pthread.h>
+
+
+
+gboolean timeout(gpointer data)
+{
+	int *n = (int*)data;
+	g_debug("[%ld] -timeout- receive %d\n", pthread_self(), *n);
+	*n += 1;
+	return *n < 20;
+}
+
+gboolean idle(gpointer data)
+{
+	int *n = (int*)data;
+	g_debug("[%ld] -idle- receive %d \n", pthread_self(), *n);
+	*n += 1;
+	return *n < 10;
+}
+
+gboolean source_timeout(gpointer data)
+{
+	static int index = 0;
+	index++;
+	g_debug("[%ld] - source_tiemout - monitor - %d\n", pthread_self(), index);
+	if(index == 5)
+	{
+		g_main_loop_quit((GMainLoop*)data);
+		return FALSE;
+	}
+	return TRUE;
+}
+
+void create_monitor_source(GMainContext *context, GMainLoop *loop)
+{
+	GSource *source = g_timeout_source_new(2000);
+	int id = g_source_attach(source, context);
+	g_source_set_callback(source, source_timeout, loop, NULL);
+}
 
 int main(int argc, char* argv[])
 {
-	g_debug("hello world\n");
+	int n = 0;
+	GMainContext *context = g_main_context_default();
+	g_debug("[%ld] hello world\n", pthread_self());
+	GMainLoop *loop = g_main_loop_new(context, 0);
+	create_monitor_source(context, loop);
+	g_timeout_add(1000, timeout, &n);
+	g_idle_add(idle, &n);
+	g_main_loop_run(loop);
 	return 0;
 }
